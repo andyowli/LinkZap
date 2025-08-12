f0186ffb38bceb41ab3f8a75cacb3fcbfc1c62c1
andyowli <382719724@qq.com>
2025-08-12 18:49:13 +0800

    Modify the file again

--- src/components/empty-data.tsx
+++ src/components/empty-data.tsx
@@ -2,21 +2,16 @@
 
 import { RotateCw } from "lucide-react";
 import { Button } from "./ui/button";
-import Image from "next/image";
+import { Empty } from 'antd';
 
 export const EmptyData = () => {
     return (
         <div className="h-[80vh] flex flex-col items-center justify-center">
-            <Image
-                src="/data.png"
-                alt="No data"
-                width={300}
-                height={300}
-                className="mb-4"
-            />
-            <div className="flex flex-col items-center justify-center mb-8 mt-[-60]">
-                <h2>No data yet</h2>
-                <p className="text-[#6B7280]">There is no content on this page. Please try again later.</p>
+            <div className="mb-8">
+                <Empty 
+                    description={<span className="dark:text-[#6B7280] text-gray-600">No Data</span>}
+                />
+                <p className="text-[#6B7280]">There is no content on this page,please try again later.</p>
             </div>
 
             <Button 
